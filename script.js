0e964fe0e5e059d9bcae2da78b14ca91e1ce3ce2
Varun Pal <palvarun8368@gmail.com>
2025-06-07 17:05:59 +0530

    navbar changes

--- script.js
+++ script.js
@@ -7,3 +7,16 @@
     backDelay: 1000,
     loop:true
   });
+
+    
+    const menuBtn = document.querySelector('.menu-btn');
+  const navLinks = document.querySelector('.links ul');
+
+  menuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+  });
+  
+
+
+
+
